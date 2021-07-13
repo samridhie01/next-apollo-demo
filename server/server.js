9e5a9659adc585ffb9d5983e735ebba8fcdbf888
Samridhie Gambhir <samridhie1@gmail.com>
2021-07-14 00:59:43 +0530

    Samridhie | Updated dependencies in server and used latest apollo graphql code

--- server/server.js
+++ server/server.js
@@ -1,20 +1,48 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
-const myGraphQLSchema = require('./schema')
+// const nameSchema  = require('./schema');
+// const nameResolver = require('./resolver');
+const { ApolloServer, gql } = require('apollo-server-express');
+const cors = require('cors');
+var casual = require('casual');
+
+const typeDefs= gql`
+  type Query {
+    name: String,
+    users: [User]
+  }
+  type User{
+    name: String,
+    addr: String
+  }
+`;
+
+
+const resolvers = {
+  Query:{
+    name: ()=>casual.name,
+    users: ()=>{
+      const list =[];
+      for(let i=0; i<2000; i++){
+        list.push({
+          name: casual.name,
+          addr: casual.address
+        })
+      }
+       return list;
+      }
+  }
+}
 
 const app = express();
 
-// to access graphql API from the client side
-app.use(cors())
-// bodyParser is needed just for POST.
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
-// for the graphiql interface
-app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+const server = new ApolloServer({  typeDefs: typeDefs, resolvers: resolvers, introspection: true, playground: true});
 
+app.use(cors())
+
+
+server.applyMiddleware({app});
 const port = process.env.PORT || 5000
 app.listen(port, (err) => {
   if (err) throw err
-  console.log(`Graphql Server started on: http://localhost:${port}`)
+  console.log(`Graphql Server started on: http://localhost:${port}${server.graphqlPath}`)
 })
