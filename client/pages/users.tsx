619572109bdea72fb33b1f379270c41daefc0de7
Samridhie Gambhir <samridhie1@gmail.com>
2021-07-20 19:01:41 +0530

    Samridhie | added config for environment and go back link to users

--- client/pages/users.tsx
+++ client/pages/users.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ClientOnly from "../components/ClientOnly/ClientOnly";
-
+import Link from 'next/link'
 import UserGrid from "../components/UserGrid/UserGrid";
 
 const users = () => {
     
     return(
+        <div>
+        <Link href="/"><a>Go Back</a></Link>
         <ClientOnly>
             <UserGrid />
         </ClientOnly>
+        </div>
     )
 }
 
-export default users;
+export default users;   
