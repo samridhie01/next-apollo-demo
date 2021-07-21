6b79e4950b76dfb597b6485262125f0077ab19bc
Samridhie Gambhir <samridhie1@gmail.com>
2021-07-22 00:12:14 +0530

    Samrihie | fixed tests

--- client/components/UserGrid/UserGrid.test.tsx
+++ client/components/UserGrid/UserGrid.test.tsx
@@ -1,13 +1,36 @@
 import { MockedProvider } from "@apollo/client/testing";
-import { cleanup, render } from "@testing-library/react";
+import { cleanup, fireEvent, render, waitFor } from "@testing-library/react";
 import React from "react";
 import { USERS_QUERY } from "../../queries/queries";
 import UserGrid from "./UserGrid";
+import '@testing-library/jest-dom'
 
 
 
 describe('UserGrid', () => {
-  const Wrapper = ({ mocks = [] }) => (
+  const mocks = [
+    {
+      request: {
+        query: USERS_QUERY,
+        variables:{
+          first: 2,
+          offset: 0
+        }
+      },
+      result: {
+        data: {
+          allUsers:{
+            users:[{
+              name: "name1",
+              addr: "address1"
+            }],
+            totalCount:20
+          }
+        }
+      },
+    },
+  ];
+  const Wrapper = (mocks=[]) => (
     <MockedProvider mocks={mocks} addTypename={false}>
       <UserGrid />
     </MockedProvider>
@@ -15,51 +38,22 @@ describe('UserGrid', () => {
 
   beforeEach(cleanup);
 
-  it('renders without crashing', () => {
-    const { getByTestId } = render(<Wrapper  />)
-    expect(getByTestId('user-grid')).toBeTruthy()
-  });
+
   it('renders loader on the screen', () => {
-    const { getByTestId } = render(<Wrapper />)
+    const { getByTestId } = render(Wrapper());
     expect(getByTestId('loader')).toBeTruthy();
   });
 
-   it('renders users and load button on the screen', () => {
-  
-    const mocks = [
-      {
-        request: {
-          query: USERS_QUERY,
-          variables:{
-            first: 2,
-            offset: 0
-          }
-        },
-        result: {
-          data: {
-            allUsers:{
-              users:[{
-                name: "name1",
-                addr: "address1"
-              },
-              {
-                name: "Mr. Helena Rolfson2",
-                addr: "address2"
-              }],
-              totalCount:2
-            }
-          }
-        },
-      },
-    ];
+  it('renders without crashing', async() => {
+    const { findByTestId } = render(Wrapper())
+    const userGrid  = await findByTestId('user-grid');
+    expect(userGrid).toBeInTheDocument();
+  });
 
-
-    const { findAllByTestId } = render(<Wrapper mocks={mocks} />)
-    jest.useFakeTimers();
-    setTimeout(() => {
-      expect(findAllByTestId('user-grid')).toBeInTheDocument();
-    }, 1500);
-    expect(findAllByTestId('loading-button')).toBeInTheDocument();
+   it('renders load button on the screen', async() => {
+    const { findByTestId } = render(Wrapper())
+    const loadMoreBtn  = await findByTestId('loading-button');
+    expect(loadMoreBtn).toBeInTheDocument();
   });
 
   
